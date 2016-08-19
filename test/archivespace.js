84bc68f39e976aed3a1619d923cd5ee2179ecc57
David Naughton <nihiliad@gmail.com>
2016-08-19 14:13:46 -0500

    ArchiveSpace plugin complete.

--- test/archivespace.js
+++ test/archivespace.js
@@ -1,6 +1,6 @@
 'use strict';
 const test = require('tape');
-// const cheerio = require('cheerio');
+const cheerio = require('cheerio');
 const plugin = require('../').archivespace();
 const tester = require('@nihiliad/janus/uri-factory/plugin-tester')({runIntegrationTests: false});
 
@@ -25,64 +25,66 @@ test('archivespace uriFor() missing "search" arguments', function (t) {
       scope: null,
       field: 'subject',
     },
-    /*
-    'only "scope" argument has a truthy value': {
-      search: false,
-      scope: '3', // (CBI) Charles Babbage Institute
-      field: null,
-    },
-    'both "scope" and "field" arguments have truthy values': {
-      search: 0,
-      scope: '3',
-      field: 'title',
-    },
-    */
   };
   tester.missingSearchArgs(t, plugin, testCases);
 });
 
-/*
+test('archivespace plugin uriFor() with no arguments except scope', function (t) {
+  const [warning, uri] = plugin.uriFor(false, '5', null);
+  const expectedHref = 'http://archives.lib.umn.edu/repositories/5'; // Givens Collection of African American Literature
+  t.equal(uri.href(), expectedHref, 'with no arguments except scope we get expected href, to browse the associated repository (' + expectedHref + ')...');
+  t.equal(warning, 'Missing or empty search expression.', '...and expected warning returned for a missing search expression');
+  t.end();
+});
+
+test('archivespace plugin uriFor() with valid scope and field arguments', function (t) {
+  const [warning, uri] = plugin.uriFor(0, '3', 'title');
+  const expectedHref = 'http://archives.lib.umn.edu/repositories/3'; // Chalres Babbage Institute
+  t.equal(uri.href(), expectedHref, 'with valid scope and field arguments we get expected href, to browse the associated repository, ignoring field (' + expectedHref + ')...');
+  t.equal(warning, 'Missing or empty search expression.', '...and expected warning returned for a missing search expression');
+  t.end();
+});
+
 test('archivespace invalid field args', function (t) {
-  tester.invalidFieldArgs(t, plugin, 'http://discover.lib.umn.edu/cgi/f/findaid/findaid-idx?c=umfa&cc=umfa&type=simple&q1=darwin&rgn=Entire+Finding+Aid');
+  tester.invalidFieldArgs(t, plugin, 'http://archives.lib.umn.edu/search?q=darwin');
 });
 
 test('archivespace invalid scope args', function (t) {
-  tester.invalidScopeArgs(t, plugin, 'http://discover.lib.umn.edu/cgi/f/findaid/findaid-idx?c=umfa&cc=umfa&type=simple&q1=darwin&rgn=Entire+Finding+Aid');
+  tester.invalidScopeArgs(t, plugin, 'http://archives.lib.umn.edu/search?q=darwin');
 });
 
 test('archivespace uriFor() valid "search" arguments', function (t) {
   // testCases map expectedUrl to uriFor arguments
   const testCases = {
-    'http://discover.lib.umn.edu/cgi/f/findaid/findaid-idx?c=umfa&cc=umfa&type=simple&q1=darwin&rgn=Entire+Finding+Aid': {
+    'http://archives.lib.umn.edu/search?q=darwin': {
       search: 'darwin',
       scope: null,
       field: null,
     },
-    'http://discover.lib.umn.edu/cgi/f/findaid/findaid-idx?c=umfa&cc=umfa&type=simple&q1=darwin&rgn=Subjects': {
+    'http://archives.lib.umn.edu/advanced_search?advanced=true&v0=darwin&f0=title': {
       search: 'darwin',
       scope: null,
-      field: 'subject',
+      field: 'title',
     },
-    'http://discover.lib.umn.edu/cgi/f/findaid/findaid-idx?c=umfa&cc=umfa&type=simple&q1=difference+engine&r=cbi&rgn=Entire+Finding+Aid': {
+    'http://archives.lib.umn.edu/search?q=difference+engine&filter_term%5B%5D=%7B%22repository%22%3A%22%2Frepositories%2F3%22%7D': {
       search: 'difference engine',
-      scope: 'cbi',
+      scope: '3', // CBI
       field: null,
     },
-    'http://discover.lib.umn.edu/cgi/f/findaid/findaid-idx?c=umfa&cc=umfa&type=simple&q1=minnesota&r=umja&rgn=Collection+Title': {
+    'http://archives.lib.umn.edu/advanced_search?advanced=true&v0=minnesota&f0=title&filter_term%5B%5D=%7B%22repository%22%3A%22%2Frepositories%2F15%22%7D': {
       search: 'minnesota',
-      scope: 'umja',
+      scope: '15', // UMJA
       field: 'title',
     },
   };
 
   function getResultCount (html) {
     const $ = cheerio.load(html);
-    const divs = $('div#slicehdr div.resultsheader');
-    const matches = $(divs[0]).text().trim().match(/(\d+) record/);
+    const divs = $('div.search-results > div.pull-left');
+    const matches = $(divs[0]).text().trim().match(/of (\d+) Results/);
     const count = matches.pop();
     return count;
   }
 
   tester.validSearchArgs(t, plugin, testCases, getResultCount);
 });
-*/
