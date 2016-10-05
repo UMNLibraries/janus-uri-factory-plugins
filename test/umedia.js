28b6c1c36918db014ad4d0300ae2fcf7ce4377af
David Naughton <nihiliad@gmail.com>
2016-10-05 16:32:55 -0500

    Simplifed UMedia URLs by removing unnecessary params.

--- test/umedia.js
+++ test/umedia.js
@@ -40,32 +40,32 @@ test('umedia uriFor() missing "search" arguments', function (t) {
 });
 
 test('umedia invalid field args', function (t) {
-  tester.invalidFieldArgs(t, plugin, 'https://umedia.lib.umn.edu/dasearch?term=darwin');
+  tester.invalidFieldArgs(t, plugin, 'https://umedia.lib.umn.edu/dasearch/darwin');
 });
 
 test('umedia invalid scope args', function (t) {
-  tester.invalidScopeArgs(t, plugin, 'https://umedia.lib.umn.edu/dasearch?term=darwin');
+  tester.invalidScopeArgs(t, plugin, 'https://umedia.lib.umn.edu/dasearch/darwin');
 });
 
 test('umedia uriFor() valid "search" arguments', function (t) {
   // testCases map expectedUrl to uriFor arguments
   const testCases = {
-    'https://umedia.lib.umn.edu/dasearch?term=darwin': {
+    'https://umedia.lib.umn.edu/dasearch/darwin': {
       search: 'darwin',
       scope: null,
       field: null,
     },
-    'https://umedia.lib.umn.edu/dasearch/subject:%28darwin%29?term=darwin&mode=expert': {
+    'https://umedia.lib.umn.edu/dasearch/subject:%28darwin%29?mode=expert': {
       search: 'darwin',
       scope: null,
       field: 'subject',
     },
-    'https://umedia.lib.umn.edu/dasearch/%28im_og_gid:69339%20AND%20%28darwin%29%29?term=darwin&filters=type%3Adasset+im_og_gid%3A69339&mode=expert': {
+    'https://umedia.lib.umn.edu/dasearch/%28im_og_gid:69339%20AND%20%28darwin%29%29?mode=expert': {
       search: 'darwin',
       scope: '69339', // John R. Borchert Map Library
       field: null,
     },
-    'https://umedia.lib.umn.edu/dasearch/%28im_og_gid:69339%20AND%20titles:%28darwin%29%29?term=darwin&filters=type%3Adasset+im_og_gid%3A69339&mode=expert': {
+    'https://umedia.lib.umn.edu/dasearch/%28im_og_gid:69339%20AND%20titles:%28darwin%29%29?mode=expert': {
       search: 'darwin',
       scope: '69339', // John R. Borchert Map Library
       field: 'title',
