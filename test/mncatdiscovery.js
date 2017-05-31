c7c30e5f0927ac08a331acb0e0ed99c6397ad609
David Naughton <nihiliad@gmail.com>
2017-05-31 12:17:23 -0500

    Updated MNCATDiscovery to use https. Bumped version to 0.6.0. Fixes #9.

--- test/mncatdiscovery.js
+++ test/mncatdiscovery.js
@@ -5,11 +5,11 @@ const plugin = require('../').mncatdiscovery();
 const tester = require('@nihiliad/janus/uri-factory/plugin-tester')({runIntegrationTests: false});
 
 test('mncatdiscovery baseUri()', function (t) {
-  tester.baseUri(t, plugin, 'http://primo.lib.umn.edu/primo_library/libweb/action/dlSearch.do?institution=TWINCITIES&vid=TWINCITIES&indx=1&dym=true&highlight=true&lang=eng');
+  tester.baseUri(t, plugin, 'https://primo.lib.umn.edu/primo_library/libweb/action/dlSearch.do?institution=TWINCITIES&vid=TWINCITIES&indx=1&dym=true&highlight=true&lang=eng');
 });
 
 test('mncatdiscovery emptySearchUri()', function (t) {
-  tester.emptySearchUri(t, plugin, 'http://primo.lib.umn.edu/primo_library/libweb/action/search.do?institution=TWINCITIES&vid=TWINCITIES&indx=1&dym=true&highlight=true&lang=eng');
+  tester.emptySearchUri(t, plugin, 'https://primo.lib.umn.edu/primo_library/libweb/action/search.do?institution=TWINCITIES&vid=TWINCITIES&indx=1&dym=true&highlight=true&lang=eng');
 });
 
 test('mncatdiscovery uriFor() missing "search" arguments', function (t) {
@@ -40,32 +40,32 @@ test('mncatdiscovery uriFor() missing "search" arguments', function (t) {
 });
 
 test('mncatdiscovery invalid field args', function (t) {
-  tester.invalidFieldArgs(t, plugin, 'http://primo.lib.umn.edu/primo_library/libweb/action/dlSearch.do?institution=TWINCITIES&vid=TWINCITIES&indx=1&dym=true&highlight=true&lang=eng&search_scope=mncat_discovery&query=any%2Ccontains%2Cdarwin');
+  tester.invalidFieldArgs(t, plugin, 'https://primo.lib.umn.edu/primo_library/libweb/action/dlSearch.do?institution=TWINCITIES&vid=TWINCITIES&indx=1&dym=true&highlight=true&lang=eng&search_scope=mncat_discovery&query=any%2Ccontains%2Cdarwin');
 });
 
 test('mncatdiscovery invalid scope args', function (t) {
-  tester.invalidScopeArgs(t, plugin, 'http://primo.lib.umn.edu/primo_library/libweb/action/dlSearch.do?institution=TWINCITIES&vid=TWINCITIES&indx=1&dym=true&highlight=true&lang=eng&search_scope=mncat_discovery&query=any%2Ccontains%2Cdarwin');
+  tester.invalidScopeArgs(t, plugin, 'https://primo.lib.umn.edu/primo_library/libweb/action/dlSearch.do?institution=TWINCITIES&vid=TWINCITIES&indx=1&dym=true&highlight=true&lang=eng&search_scope=mncat_discovery&query=any%2Ccontains%2Cdarwin');
 });
 
 test('mncatdiscovery uriFor() valid "search" arguments', function (t) {
   // testCases map expectedUrl to uriFor arguments
   const testCases = {
-    'http://primo.lib.umn.edu/primo_library/libweb/action/dlSearch.do?institution=TWINCITIES&vid=TWINCITIES&indx=1&dym=true&highlight=true&lang=eng&search_scope=mncat_discovery&query=any%2Ccontains%2Cdarwin': {
+    'https://primo.lib.umn.edu/primo_library/libweb/action/dlSearch.do?institution=TWINCITIES&vid=TWINCITIES&indx=1&dym=true&highlight=true&lang=eng&search_scope=mncat_discovery&query=any%2Ccontains%2Cdarwin': {
       search: 'darwin',
       scope: null,
       field: null,
     },
-    'http://primo.lib.umn.edu/primo_library/libweb/action/dlSearch.do?institution=TWINCITIES&vid=TWINCITIES&indx=1&dym=true&highlight=true&lang=eng&search_scope=mncat_discovery&query=sub%2Ccontains%2Cdarwin': {
+    'https://primo.lib.umn.edu/primo_library/libweb/action/dlSearch.do?institution=TWINCITIES&vid=TWINCITIES&indx=1&dym=true&highlight=true&lang=eng&search_scope=mncat_discovery&query=sub%2Ccontains%2Cdarwin': {
       search: 'darwin',
       scope: null,
       field: 'subject',
     },
-    'http://primo.lib.umn.edu/primo_library/libweb/action/dlSearch.do?institution=TWINCITIES&vid=TWINCITIES&indx=1&dym=true&highlight=true&lang=eng&search_scope=wilson_rare&query=any%2Ccontains%2Cfrancis+scott+fitzgerald': {
+    'https://primo.lib.umn.edu/primo_library/libweb/action/dlSearch.do?institution=TWINCITIES&vid=TWINCITIES&indx=1&dym=true&highlight=true&lang=eng&search_scope=wilson_rare&query=any%2Ccontains%2Cfrancis+scott+fitzgerald': {
       search: 'francis scott fitzgerald',
       scope: 'wilson_rare',
       field: null,
     },
-    'http://primo.lib.umn.edu/primo_library/libweb/action/dlSearch.do?institution=TWINCITIES&vid=TWINCITIES&indx=1&dym=true&highlight=true&lang=eng&search_scope=givens&query=title%2Ccontains%2Cinvisible+man': {
+    'https://primo.lib.umn.edu/primo_library/libweb/action/dlSearch.do?institution=TWINCITIES&vid=TWINCITIES&indx=1&dym=true&highlight=true&lang=eng&search_scope=givens&query=title%2Ccontains%2Cinvisible+man': {
       search: 'invisible man',
       scope: 'givens',
       field: 'title',
