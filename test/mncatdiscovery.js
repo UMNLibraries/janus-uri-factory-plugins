286c1e213b2446ec0f8ec16b91a9bc2f7ef157b3
David Naughton <nihiliad@gmail.com>
2019-08-29 14:32:32 -0500

    Changes 'primoui' to 'primo' in a comment.

--- test/mncatdiscovery.js
+++ test/mncatdiscovery.js
@@ -3,7 +3,7 @@ const test = require('tape');
 const plugin = require('../').mncatdiscovery();
 const tester = require('@nihiliad/janus/uri-factory/plugin-tester')({runIntegrationTests: false});
 
-// Simply validate that a primoui test (to which mncatdiscovery is aliased) runs
+// Simply validate that a primo (to which mncatdiscovery is aliased) test runs
 test('mncatdiscovery baseUri()', function (t) {
   tester.baseUri(t, plugin, 'https://primo.lib.umn.edu/primo-explore/search?institution=TWINCITIES&vid=TWINCITIES&dum=true&highlight=true&lang=en_US');
 });
