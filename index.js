43a8de07a8f345fcea4d7e48138fe2009a743cf7
John Barneson <barne102@umn.edu>
2025-03-20 13:24:20 -0500

    Updated plugin modules to esm

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 'use strict'
-const path = require('path')
-module.exports = require('require-all')(path.resolve(__dirname, 'lib'))
+import path from 'path';
+const directory = './lib';
+const moduleMap = {};
+const files = fs.readdir(directory);
+
+for (const file of files) {
+  if (file.endsWith('.js')) {
+    const modulePath = path.join(directory, file);
+    try {
+      const module = import(modulePath);
+      moduleMap[file.slice(0, -3)] = module;
+    } catch (error) {
+      console.error(`Error importing ${file}:`, error);
+    }
+  }
+}
+export default moduleMap;
+
